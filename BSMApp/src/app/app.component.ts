d36686ce8e49417dcee918cb610b1d96ec9c1e6a
ENOSISBD\snahashis <snahashis.shuvon@enosisbd.com>
2024-05-15 14:30:19 +0600

    display image

--- BSMApp/src/app/app.component.ts
+++ BSMApp/src/app/app.component.ts
@@ -3,13 +3,15 @@ import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angul
 import { HomePageComponent } from "./home-page/home-page.component";
 import { ReactiveFormsModule } from '@angular/forms';
 import { BookDetailsFormComponent } from "./book-details-form/book-details-form.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HomePageComponent, ReactiveFormsModule, BookDetailsFormComponent, RouterModule, RouterLink, RouterLinkActive]
+    imports: [RouterOutlet, HomePageComponent, ReactiveFormsModule, BookDetailsFormComponent, RouterModule, RouterLink, RouterLinkActive, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'BSMApp';
